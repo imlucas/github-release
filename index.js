69421c8ab8f348b9776c4d5b4e72416b9ef5742a
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-07-16 00:10:55 -0400

    more usabled outside of gulp

--- index.js
+++ index.js
@@ -7,27 +7,6 @@ var request = require('request'),
   mime = require('mime'),
   debug = require('debug')('github-release');
 
-module.exports = function(pkg){
-  var repo = /git:\/\/github.com\/([\w\/\-]+)\.git/.exec(pkg.repository.url)[1],
-    token = process.env.GITHUB_TOKEN,
-    tag = 'v' + pkg.version,
-    src,
-    dest;
-
-  return through.obj(function(file, enc, fn){
-    src = file.path;
-    dest = path.basename(src);
-
-    upload(token, repo, tag, src, dest, function(err, res){
-      if(err) return fn(err);
-
-      debug('Uploaded asset for release ' + tag, res);
-      this.push(file);
-      fn();
-    }.bind(this));
-  });
-};
-
 
 function upload(token, repo, tag, src, dest, fn){
   debug('upload', {repo: repo, tag: tag, src: src, dest: dest});
@@ -98,3 +77,28 @@ function getReleases(token, repo, fn){
     fn(null, JSON.parse(body));
   });
 }
+
+module.exports = function(pkg){
+  var repo = /git:\/\/github.com\/([\w\/\-]+)\.git/.exec(pkg.repository.url)[1],
+    token = process.env.GITHUB_TOKEN,
+    tag = 'v' + pkg.version,
+    src,
+    dest;
+
+  return through.obj(function(file, enc, fn){
+    src = file.path;
+    dest = path.basename(src);
+
+    upload(token, repo, tag, src, dest, function(err, res){
+      if(err) return fn(err);
+
+      debug('Uploaded asset for release ' + tag, res);
+      this.push(file);
+      fn();
+    }.bind(this));
+  });
+};
+
+module.exports.create = createRelease;
+module.exports.list = getReleases;
+module.exports.upload = upload;
